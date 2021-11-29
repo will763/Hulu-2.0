3ae1524a79624016b060097e1a5cab9568e0732e
will <killzasper@gmail.com>
2021-11-29 00:24:24 -0300

    fix: BASE_URL error name

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,7 +4,7 @@ import { forwardRef } from "react"
  
  // eslint-disable-next-line react/display-name
  const Thumbnail = forwardRef(({result},ref) => {
-    const BASE_URL = "https://Image.tmdb.org/t/p/original/"      
+    const BASE_URL = "https://image.tmdb.org/t/p/original/"      
 
     return(
         <div ref={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
